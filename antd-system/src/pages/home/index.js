75288f7d3130cafa395e878eb12d9a003f2cdcc5
EchOneJY <715809341@qq.com>
2020-01-03 22:50:19 +0800

    1

--- antd-system/src/pages/home/index.js
+++ antd-system/src/pages/home/index.js
@@ -1,71 +1,99 @@
 import React from 'react'
 import { Input, Button, List } from 'antd'
+import store from '@/store'
 import { connect } from 'react-redux'
 import {
   changeInputAction,
   addItemAction,
-  deleteItemAction,
-  getMyListAction
+  deleteItemAction
 } from '@/store/actionCreators'
 
-const Home = ({ ...props }) => {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = store.getState()
-  //   store.subscribe(this.storeChange)
-  // }
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+    store.subscribe(this.storeChange)
+  }
 
-  // storeChange = () => {
-  //     this.setState(store.getState())
-  // }
+  storeChange = () => {
+    this.setState(store.getState())
+  }
 
-  // changeInputVal = (e) => {
-  //     const action = changeInputAction(e.target.value)
-  //     store.dispatch(action)
-  // }
+  changeInputVal = e => {
+    const action = changeInputAction(e.target.value)
+    store.dispatch(action)
+  }
 
-  // addEvents = () => {
-  //     const action = addItemAction
-  //     store.dispatch(action)
-  // }
+  addEvents = () => {
+    const action = addItemAction()
+    store.dispatch(action)
+  }
 
-  // deleteItem = (idx) => {
-  //     const action = deleteItemAction(idx)
-  //     store.dispatch(action)
-  // }
+  deleteItem = idx => {
+    const action = deleteItemAction(idx)
+    store.dispatch(action)
+  }
 
-  React.useEffect(() => {
-    getMyListAction()
-  }, [])
-  //const { inputValue, changeInputVal, addEvents, list, deleteItem } = props
-  return (
-    <div>
+  render() {
+    return (
       <div>
-        <Input
-          value={inputValue}
-          placeholder="Write Something"
-          style={{ width: '250px', marginRight: '10px' }}
-          onChange={changeInputAction}
-        />
-        <Button type="primary" onClick={addItemAction}>
-          增加
-        </Button>
+        <div>
+          <Input
+            value={this.state.todos.inputValue}
+            placeholder="Write Something"
+            style={{ width: '250px', marginRight: '10px' }}
+            onChange={this.changeInputVal}
+          />
+          <Button type="primary" onClick={this.addEvents}>
+            增加
+          </Button>
+        </div>
+        <div style={{ marginTop: '20px', width: '300px' }}>
+          <List
+            bordered
+            //关键代码-----------start
+            dataSource={this.state.todos.list}
+            //关键代码-----------end
+            renderItem={(item, index) => (
+              <List.Item onClick={() => this.deleteItem(index)}>
+                {item}
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
-      <div style={{ marginTop: '20px', width: '300px' }}>
-        <List
-          bordered
-          //关键代码-----------start
-          dataSource={list}
-          //关键代码-----------end
-          renderItem={(item, index) => (
-            <List.Item onClick={() => deleteItemAction(index)}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
-    </div>
-  )
+    )
+  }
+
+  //const { inputValue, changeInputVal, addEvents, list, deleteItem } = props
+  // return (
+  //   <div>
+  //     <div>
+  //       <Input
+  //         value={inputValue}
+  //         placeholder="Write Something"
+  //         style={{ width: '250px', marginRight: '10px' }}
+  //         onChange={changeInputAction}
+  //       />
+  //       <Button type="primary" onClick={addItemAction}>
+  //         增加
+  //       </Button>
+  //     </div>
+  //     <div style={{ marginTop: '20px', width: '300px' }}>
+  //       <List
+  //         bordered
+  //         //关键代码-----------start
+  //         dataSource={list}
+  //         //关键代码-----------end
+  //         renderItem={(item, index) => (
+  //           <List.Item onClick={() => deleteItemAction(index)}>
+  //             {item}
+  //           </List.Item>
+  //         )}
+  //       />
+  //     </div>
+  //   </div>
+  // )
 }
 
 // const stateToProps = state => {
--- antd-system/src/store/sagas.js
+++ antd-system/src/store/sagas.js
@@ -1,27 +1,23 @@
 import { takeEvery, put } from 'redux-saga/effects'
-import { GET_MY_LIST } from './actionTypes'
-import { getListAction } from './actionCreatores'
-import axios from 'axios'
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './actionTypes'
+import {
+  changeInputAction,
+  addItemAction,
+  deleteItemAction
+} from './actionCreators'
+//import axios from 'axios'
 
 //generator函数
-function* mySaga() {
+function* rootSagas() {
   //等待捕获action
-  yield takeEvery(GET_MY_LIST, getList)
+  yield takeEvery(CHANGE_INPUT, changeInputAction)
+  yield takeEvery(ADD_ITEM, addItem)
+  yield takeEvery(DELETE_ITEM, deleteItemAction)
 }
 
-function* getList() {
-  //这段代码我就不删除了。
-  // axios.get('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList').then((res)=>{
-  //     const data = res.data
-  //     const action = getListAction(data)
-  //     put(action)
-
-  // })
-  const res = yield axios.get(
-    'https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList'
-  )
-  const action = getListAction(res.data)
-  yield put(action)
+function* addItem() {
+  console.log(1)
+  // yield put(addItemAction())
 }
 
-export default mySaga
+export default rootSagas
