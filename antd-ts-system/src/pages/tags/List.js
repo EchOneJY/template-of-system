8cfde70a90a451b9f7754ddb38e54873b960f577
EchOneJY <715809341@qq.com>
2020-01-13 00:59:58 +0800

    1

--- antd-ts-system/src/pages/tags/List.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import CategoryAndTags from 'components/CategoryAndTags'
-
-const TagsList = () => {
-  return <CategoryAndTags showType="1" />
-}
-
-export default TagsList
