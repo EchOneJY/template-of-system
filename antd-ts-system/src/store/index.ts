c9984f1bfcf0cbffd7fb7b31c44ef6c7f6c91776
EchOneJY <715809341@qq.com>
2020-01-19 22:05:54 +0800

    1

--- antd-ts-system/src/store/index.ts
+++ antd-ts-system/src/store/index.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import createSagaMiddleware from 'redux-saga'
-import mySaga from './sagas'
+// import mySaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
@@ -11,6 +11,6 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
-sagaMiddleware.run(mySaga)
+// sagaMiddleware.run(mySaga)
 
 export default store
